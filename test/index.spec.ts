456460c2ca24ff23c69a855ba84e0e5ab3ebab4f
brecert <11599528+Brecert@users.noreply.github.com>
2019-06-21 03:23:55 -0400

    added tests

---
+++ test/index.spec.ts
@@ -0,0 +1,7 @@
+import Keysdown from '../src/index'
+
+describe('Keysdown', () => {
+	it('should be defined', () => {
+		expect(Keysdown).toBeDefined
+	})
+})
---
+++ test/keysdown.spec.ts
@@ -0,0 +1,29 @@
+import Keysdown from '../src/keysdown'
+
+describe('Keysdown', () => {
+	let keysdown: Keysdown
+	
+	it('should be created with the window as a target', () => {
+		keysdown = new Keysdown(window)
+		expect(keysdown).toBeDefined
+		expect(keysdown.target).toEqual(window)
+	})
+
+	it('should hold the "k" key', () => {
+		let k_down_event = new KeyboardEvent('keydown', {
+			key: 'k'
+		})
+		window.dispatchEvent(k_down_event)
+
+		expect(keysdown.has('k')).toBe(true)
+	})
+
+	it('should release the "k" key', () => {
+		let k_up_event = new KeyboardEvent('keyup', {
+			key: 'k'
+		})
+		window.dispatchEvent(k_up_event)
+
+		expect(keysdown.has('k')).toBe(false)
+	})
+})
